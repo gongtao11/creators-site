2d4b0ba06c0f1b29c6d28ab7ff36499492de8145
gongtao11 <979670278@qq.com>
2026-08-03 13:55:25 +0800

    Clean encoding - remove Chinese comments

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// Proxy: 姣忎釜椤甸潰鍐呴儴鑷妫€鏌ョ櫥褰曠姸鎬侊紝杩欓噷鍙仛杞彂
+
 export function proxy(request: NextRequest) {
   return NextResponse.next();
 }
